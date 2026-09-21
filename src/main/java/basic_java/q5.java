13c60b2bb8721b5033ddc3c394a7bf63f646849d
vedanshagrahari217 <vedanshagrahari217@gmail.com>
2026-09-21 11:00:18 +0530

    add stop word filtered word frequency

---
+++ src/main/java/basic_java/q5.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class q5 {
+
+    static void printFilteredWordFrequency(String feedback) {
+
+        feedback = feedback.toLowerCase();
+
+        feedback = feedback.replace(".", "");
+        feedback = feedback.replace(",", "");
+
+        String[] words = feedback.split("\\s+");
+
+        String[] stopWords = {"the", "was", "and", "a", "is", "of", "in"};
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < words.length; i++) {
+
+            boolean isStopWord = false;
+
+            for (int j = 0; j < stopWords.length; j++) {
+                if (words[i].equals(stopWords[j])) {
+                    isStopWord = true;
+                    break;
+                }
+            }
+
+            if (!isStopWord) {
+                if (map.containsKey(words[i])) {
+                    map.put(words[i], map.get(words[i]) + 1);
+                } else {
+                    map.put(words[i], 1);
+                }
+            }
+        }
+
+        ArrayList<Map.Entry<String, Integer>> list =
+                new ArrayList<>(map.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+
+            public int compare(Map.Entry<String, Integer> a,
+                               Map.Entry<String, Integer> b) {
+
+                return b.getValue() - a.getValue();
+            }
+        });
+
+        for (Map.Entry<String, Integer> entry : list) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String feedback = sc.nextLine();
+
+        printFilteredWordFrequency(feedback);
+
+        sc.close();
+    }
+}
