a378f75d43d384f4590aed9b157cd649b2c37870
vedanshagrahari217 <vedanshagrahari217@gmail.com>
2026-09-25 13:10:31 +0530

    question 2

---
+++ src/main/java/basic_java/assignment_problems/q2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class q2 {
+
+    static String findDuplicatePick(String[] playerNames) {
+
+        for (int i = 0; i < playerNames.length; i++) {
+
+            for (int j = i + 1; j < playerNames.length; j++) {
+
+                if (playerNames[i].equals(playerNames[j])) {
+                    return "Duplicate Found: " + playerNames[i];
+                }
+            }
+        }
+
+        return "No Duplicates Found";
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of players: ");
+        int n = sc.nextInt();
+
+        sc.nextLine();
+
+        String[] playerNames = new String[n];
+
+        System.out.println("Enter player names:");
+
+        for (int i = 0; i < n; i++) {
+            playerNames[i] = sc.nextLine();
+        }
+
+        String result = findDuplicatePick(playerNames);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
