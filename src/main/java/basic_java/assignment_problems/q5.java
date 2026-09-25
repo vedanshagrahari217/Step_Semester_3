549b1ffd9bbc83e1b6c7b0bd42496bcc52c7f658
vedanshagrahari217 <vedanshagrahari217@gmail.com>
2026-09-25 13:40:41 +0530

    question 5

---
+++ src/main/java/basic_java/assignment_problems/q5.java
@@ -0,0 +1,48 @@
+public class q5 {
+
+    static class Cart {
+
+        private double[] prices;
+        private int itemCount;
+        private final String cartId;
+
+        public Cart(String cartId, int size) {
+            this.cartId = cartId;
+            prices = new double[size];
+            itemCount = 0;
+        }
+
+        public void addItem(double price) {
+            if (itemCount < prices.length) {
+                prices[itemCount] = price;
+                itemCount++;
+            }
+        }
+
+        public double getTotal() {
+            double total = 0;
+
+            for (int i = 0; i < itemCount; i++) {
+                total = total + prices[i];
+            }
+
+            return total;
+        }
+
+        public int getItemCount() {
+            return itemCount;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Cart cart = new Cart("CART-5", 20);
+
+        cart.addItem(250);
+        cart.addItem(99);
+        cart.addItem(151);
+
+        System.out.println(cart.getTotal());
+        System.out.println(cart.getItemCount());
+    }
+}
