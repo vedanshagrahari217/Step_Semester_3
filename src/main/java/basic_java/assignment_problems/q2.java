6a2f7b3f59c27bf2350e70de29a3b4d452eddc27
vedanshagrahari217 <vedanshagrahari217@gmail.com>
2026-09-25 12:47:23 +0530

    question 2

---
+++ src/main/java/basic_java/assignment_problems/q2.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class q2 {
+
+    static int maxSubArray(int[] nums) {
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (currentSum + nums[i] > nums[i]) {
+                currentSum = currentSum + nums[i];
+            } else {
+                currentSum = nums[i];
+            }
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = maxSubArray(nums);
+
+        System.out.println("Maximum Subarray Sum: " + result);
+
+        sc.close();
+    }
+}
