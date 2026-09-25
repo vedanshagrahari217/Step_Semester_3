6d5c268391be8155ba6fba5759e7f7a30b26c7b3
vedanshagrahari217 <vedanshagrahari217@gmail.com>
2026-09-25 12:55:38 +0530

    question 5

---
+++ src/main/java/basic_java/q5.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class q5 {
+
+    static int findMin(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = findMin(nums);
+
+        System.out.println("Minimum element: " + result);
+
+        sc.close();
+    }
+}
